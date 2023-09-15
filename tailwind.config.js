55a2356ce0b0bf8c701fc7d2b214ae8e2ad2d4a1
olalekan <olamilekanadekanmbi@gmail.com>
2023-09-15 04:44:56 +0100

    done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./**/*.{html,js}"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"hero-image": "url('image/h1-city-beach-discount-2023-hn-ar.jpg')",
+			},
+		},
+	},
+	plugins: [],
+};
